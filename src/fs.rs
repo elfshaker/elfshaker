43dd9bcb9994a8f6edd65c792335815b0a36b838
bolshoytoster <toasterbig@gmail.com>
2021-11-26 19:15:50 +0000

    open_file can support anything coercable to Path

--- src/fs.rs
+++ src/fs.rs
@@ -1,6 +1,6 @@
 /// Opens a file and returns a useful error if it fails
 /// If possible use this instead of File::open for usability purposes
-pub fn open_file(path: &std::path::Path) -> std::io::Result<std::fs::File> {
+pub fn open_file<P: AsRef<std::path::Path>>(path: P) -> std::io::Result<std::fs::File> {
     match std::fs::File::open(&path) {
         Err(why) => Err(std::io::Error::new(why.kind(), format!("couldn't open {}", path.display()))),
         Ok(file) => Ok(file),

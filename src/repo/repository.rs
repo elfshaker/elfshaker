75fea3cb930f383ace8b5e5cbf05618445457c32
Michal Jires <mjires@suse.cz>
2024-01-22 19:47:52 +0100

    no permissions().readonly()

--- src/repo/repository.rs
+++ src/repo/repository.rs
@@ -139,6 +139,7 @@ pub struct Repository {
     progress_reporter_factory: Box<dyn Fn(&str) -> ProgressReporter<'static> + Send + Sync>,
     /// The repository mutex file. This file is locked and unlocked
     /// when the repository instance is created/destroyed.
+    /// None represents read-only repository.
     lock_file: Option<fs::File>,
     is_locked_exclusively: AtomicBool,
 }
@@ -181,8 +182,6 @@ impl Repository {
             return Err(Error::RepositoryNotFound);
         }
 
-        let readonly = fs::metadata(&data_dir)?.permissions().readonly();
-
         let lock_file = match fs::File::create(data_dir.join("mutex")) {
             Ok(lock_file) => {
                 if let Err(e) = lock_file.try_lock_shared() {
@@ -196,7 +195,7 @@ impl Repository {
 
                 Some(lock_file)
             }
-            Err(e) if readonly && e.kind() == io::ErrorKind::PermissionDenied => None,
+            Err(e) if e.kind() == io::ErrorKind::PermissionDenied => None,
             Err(e) => Err(e)?,
         };
 
